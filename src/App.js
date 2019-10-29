c1592540b125a145ebca642835258b1b646dd737
Morgan Qin <morganqin@activenetwork.com>
2019-10-29 14:12:12 +0800

    chore: add form to trigger rechaptcha validation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <form action="?" method="POST">
-          <div className="g-recaptcha" data-sitekey="6LeqQb8UAAAAAE17lRUVfXNsYmjT6rDx22GqEwZ7"></div>
-          <br/>
-          <input type="submit" value="Submit" />
-        </form>
-      </header>
+      <form action="?" method="POST">
+        <div className="form-item">
+          <div className="form-item__label">Account:</div>
+          <input name="name" className="input__name" />
+        </div>
+        <div className="form-item">
+          <div className="form-item__label">Password:</div>
+          <input name="password" type="password" className="input__password" />
+        </div>
+        <div className="g-recaptcha" data-sitekey="6LeqQb8UAAAAAE17lRUVfXNsYmjT6rDx22GqEwZ7" />
+        <div className="form-item">
+          <input type="submit" value="Submit" className="input__submit" />
+        </div>
+      </form>
     </div>
   );
 }
